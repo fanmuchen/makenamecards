c196cad1c2eea2be2a660ee3d213751833c1693f
fanmuchen <fmc222813@gmail.com>
2024-11-06 17:29:38 +0800

    Sync design.

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,23 @@ const App: React.FC = () => {
           }}
         >
           <h2 style={{ textAlign: 'center', marginBottom: '16px' }}>席卡批量生成工具</h2>
+          <div style={{ marginTop: '16px' }}>
+              <Text type="secondary">说明：</Text>
+              <Text>
+                <ol>
+                  <li>仅支持A4纸；</li>
+                  <li>名单中的名字之间应由逗号、顿号、斜杠或回车分隔；</li>
+                  <li>目前仅支持二至四字中文姓名，输入其他字符格式会乱；</li>
+                  <li>设置好席卡纸的宽度、高度（厘米）后点击按钮生成PDF；</li>
+                  <li>打印PDF前请仔细确认打印的页边距设置；<br />
+                    <Text type="danger" style={{ lineHeight: 1 }}>
+                      涉密pdf查看器：打印→页面处理→页面缩放，选择“无”；
+                    </Text>
+                  </li>
+                  <li>不支持华文行楷不含的的生僻字。←待完善</li>
+                </ol>
+              </Text>
+            </div>
           <Form layout="vertical">
             <Form.Item label="输入名单">
               <Input.TextArea
@@ -86,34 +103,6 @@ const App: React.FC = () => {
                 生成PDF
               </Button>
             </Form.Item>
-            <div style={{ marginTop: '16px' }}>
-              <Text type="secondary">指南：</Text>
-              <Text>
-                <ol>
-                  <li>仅支持A4纸；</li>
-                  <li>名单中的名字之间应由逗号、顿号、斜杠或回车分隔；</li>
-                  <li>目前仅支持二至四字中文姓名，输入其他字符格式会乱；</li>
-                  <li>设置好席卡纸的宽度、高度（厘米）后点击按钮生成PDF；</li>
-                  <li>打印PDF前请仔细确认打印的页边距设置；<br />
-                    <Text type="danger" style={{ lineHeight: 1 }}>
-                      涉密pdf查看器：打印→页面处理→页面缩放，选择“无”；
-                    </Text>
-                  </li>
-                  <li>不支持华文行楷不含的的生僻字。←待完善</li>
-                </ol>
-              </Text>
-            </div>
-            <div style={{ marginTop: '16px' }}>
-              <Text type="secondary">声明：</Text>
-              <br />
-              <Text>
-                <ol>
-                  <li>本工具由FMC开发，任何人可自由使用；</li>
-                  <li>本工具在法律允许范围内不提供任何保障；</li>
-                  <li>沪ICP备2024055006号-1</li>
-                </ol>
-              </Text>
-            </div>
           </Form>
         </div>
       </Content>
